2f451a1b0b2b4297486d8e655f773eea1a0ed02d
NeryC <nalbertoc132@gmail.com>
2024-03-25 00:24:01 -0400

    habilitar uso de cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const routerApi = require("./routes");
+const cors = require("cors");
 
 const { errorHadler, logErrors, boomErrorHandler } = require("./error.handler");
 
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hola!");
